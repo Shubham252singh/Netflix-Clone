551ae0affe660c05b1894ae92280006f16ab88ae
Shubham252singh <shubham252singh@gmail.com>
2025-08-28 22:52:19 +0530

    made GPT api more UI oriented passing the schema and added webseries recommendation as welll with movies

--- src/components/GptSearcher.js
+++ src/components/GptSearcher.js
@@ -1,36 +1,71 @@
 import {useRef,useState} from 'react'
 import ai from '../utils/googleGenAI';
 import { useDispatch, useSelector } from 'react-redux';
-import { addRecommendedMovies, addRecommendedMoviesDtls } from '../utils/genAISlice';
+import { addRecommendedMoviesDtls, addRecommendedSeries } from '../utils/genAISlice';
 import { API_OPTIONS } from '../utils/constant';
 import { MovieList } from './MovieList';
 import Loader from './Loader';
+import { Type } from '@google/genai';
 
 const GptSearcher = () => {
     const dispatch = useDispatch();
     const [loaderVal,setLoaderVal]=useState(false);
     const searchInput = useRef(null);
     const suggestedMovie = useSelector(state=>state.genAImovies.recommendedMoviesDtls);
+    const suggestedAISeries = useSelector(state=>state.genAImovies.recommendedSeries);
     const getGenAIMovies = async (movie_name) => {
         const data = await fetch('https://api.themoviedb.org/3/search/movie?query=' + movie_name + '&include_adult=false&language=en-US&page=1', API_OPTIONS);
         const json = await data.json();
         const moviesList = json?.results.filter((element) => element.title === movie_name);
         return moviesList;
     }
+    const getGenAITVSeries = async (series_name) => {
+        const data = await fetch('https://api.themoviedb.org/3/search/tv?query=' + series_name + '&include_adult=false&language=en-US&page=1', API_OPTIONS);
+        const json = await data.json();
+        const seriesList = json?.results.filter((element) => element.name === series_name);
+        return seriesList;
+    }
 
     const handleGPTSearch = async() => {
         setLoaderVal(true);
-        const srch_filter = 'You are a movie recommender. Need to recommed only 8 movies names of the genere '+ searchInput.current.value +'.The 8 output movie names should be comma seperated'; 
+        const srch_filter = `You are a movie and web series recommender. Recommend at most 8 movies and 8 web series of the genre "${searchInput.current.value}". The recommended movie names must be present in the movieList array, and the web series names must be present in the tvseriesList array.`; 
         const response = await ai.models.generateContent({
             model: "gemini-2.5-flash",
-            contents: srch_filter,
+            contents: srch_filter, 
+            config: {
+                responseMimeType: "application/json",
+                responseSchema: {
+                    type: Type.ARRAY,
+                    items: {
+                        type: Type.OBJECT,
+                        properties: {
+                            movieList: {
+                                type: Type.ARRAY,
+                                items: {
+                                    type: Type.STRING,
+                                },
+                            },
+                            tvseriesList: {
+                                type: Type.ARRAY,
+                                items: {
+                                    type: Type.STRING,
+                                },
+                            },
+                        },
+                        propertyOrdering: ["movieList", "tvseriesList"],
+                    },
+                },
+            },
         });
-        const aiMovies = (response?.candidates[0]?.content?.parts[0].text).split(",").map((element)=>element.trim());
-        if(!aiMovies) {setLoaderVal(false); return};
-        const promiseList= aiMovies.map((movie_name)=>getGenAIMovies(movie_name));
-        const movieDtls = (await Promise.all(promiseList)).reduce((acc, val) => acc.concat(val), []);
+        const {movieList : aiMovies, tvseriesList } = JSON.parse(response?.candidates[0]?.content?.parts[0].text)[0];
+        if(!aiMovies && !tvseriesList) {setLoaderVal(false); return};
+        const promiseListMovie= aiMovies.map((movie_name)=>getGenAIMovies(movie_name));
+        const movieDtls = (await Promise.all(promiseListMovie)).reduce((acc, val) => acc.concat(val), []);
+        const promiseListSeries= tvseriesList.map((series_name)=>getGenAITVSeries(series_name));
+        const seriesDtls = (await Promise.all(promiseListSeries)).reduce((acc, val) => acc.concat(val), []);
         //if(movieDtls) {setLoaderVal(false); return};
         dispatch(addRecommendedMoviesDtls(movieDtls));
+        dispatch(addRecommendedSeries(seriesDtls));
         setLoaderVal(false);
     }
 
@@ -43,14 +78,21 @@ const GptSearcher = () => {
             </form>
         </div>
           {
-              // movie and loader false
               loaderVal && <Loader />
-              }
-          {
-              suggestedMovie && (<div className="bg-black bg-opacity-70 mt-5 py-4 h-screen">
-                  <MovieList title={"AI Suggested Movies"} movies={suggestedMovie} />
-              </div>)
           }
+          <div className='bg-black bg-opacity-70 mt-1'>
+              {
+                  suggestedMovie && (<div className="py-4 ">
+                      <MovieList title={"AI Suggested Movies"} movies={suggestedMovie} />
+                  </div>)
+              }
+              {
+                  suggestedAISeries && (<div className="py-4 ">
+                      <MovieList title={"AI Suggested Web Series"} movies={suggestedAISeries} />
+                  </div>)
+              }
+          </div>
+          
     </>
 
   )
--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,6 +1,6 @@
 const Loader = () => {
   return (
-    <div className ="absolute top-0 bg-black bg-opacity-60 flex justify-center items-center min-h-screen w-screen z-10">
+    <div className ="fixed inset-0 bg-black bg-opacity-60 flex justify-center items-center z-10">
         <div className ="loader p-4 flex rounded-xl space-x-8 ">
             <div className = "bg-red-800 rounded-full animate-bounce p-4"></div>
             <div className = "bg-red-800 rounded-full animate-bounce p-4"></div>
